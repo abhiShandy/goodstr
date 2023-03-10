12def2f2b91d2e012e43539e9798a6779021e276
abhishandy <abhishek.sandy@gmail.com>
2023-03-10 00:35:00 -0500

    feat: product overview

---
+++ client/src/ProductOverview.stories.tsx
@@ -0,0 +1,29 @@
+import ProductOverview from "./ProductOverview";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+const meta: ComponentMeta<typeof ProductOverview> = {
+  title: "ProductOverview",
+  component: ProductOverview,
+};
+
+export default meta;
+
+const Template: ComponentStory<typeof ProductOverview> = (args) => (
+  <ProductOverview {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  product: {
+    name: "Everyday Ruck Snack",
+    href: "#",
+    price: "220,000 sats",
+    description:
+      "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg",
+    imageAlt:
+      "Model wearing light green backpack with black canvas straps and front zipper pouch.",
+  },
+};
