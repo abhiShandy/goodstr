169c8295d411aec52dbc6289aeaa97436fb09147
abhishandy <abhishek.sandy@gmail.com>
2023-03-10 06:37:48 -0500

    chore: setup Github page

--- client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>React + Vite</div>;
-}
-
-export default App;
--- server/infra/server.ts
+++
@@ -1,5 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { ServerStack } from "./server-stack";
-
-const app = new cdk.App();
-new ServerStack(app, "ServerStack", {});
