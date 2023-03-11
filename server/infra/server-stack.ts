3d679e848c3b8dcb1a7b89166a4f1dcaa21ab5f4
abhishandy <abhishek.sandy@gmail.com>
2023-03-11 08:46:32 -0500

    feat: s3 bucket

--- server/infra/server-stack.ts
+++ server/infra/server-stack.ts
@@ -1,8 +1,14 @@
-import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
-export class ServerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ServerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new Bucket(this, "ProductImages", {
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
   }
 }
