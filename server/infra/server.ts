8d1784182c773f25d00254701254c9df96cd5519
abhishandy <abhishek.sandy@gmail.com>
2023-03-11 09:48:46 -0500

    chore: set stack env

--- server/infra/server.ts
+++ server/infra/server.ts
@@ -2,4 +2,9 @@ import * as cdk from "aws-cdk-lib";
 import { ServerStack } from "./server-stack";
 
 const app = new cdk.App();
-new ServerStack(app, "ServerStack", {});
+new ServerStack(app, "ServerStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
