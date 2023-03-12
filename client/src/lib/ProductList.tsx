da2087a0bd86ac5b3b01ce53ca8350a2396789dc
abhishandy <abhishek.sandy@gmail.com>
2023-03-12 15:03:03 -0400

    feat: add price unit

--- client/src/lib/ProductList.tsx
+++ client/src/lib/ProductList.tsx
@@ -46,7 +46,7 @@ export default function ProductList({
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                 <p className="mt-1 text-lg font-medium text-gray-900">
-                  {product.price}
+                  {product.price} sats
                 </p>
               </a>
             ))}
