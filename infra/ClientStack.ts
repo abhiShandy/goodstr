a62f03011ea6292e71d9a997f46dd49e7a8c7b59
abhishandy <abhishek.sandy@gmail.com>
2023-03-12 10:35:30 -0400

    feat: deploy website on cloudfront

--- infra/ClientStack.ts
+++ infra/ClientStack.ts
@@ -1,14 +1,40 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { CloudFrontWebDistribution } from "aws-cdk-lib/aws-cloudfront";
 import { Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import { join } from "path";
 
 export class ClientStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, "ClientBucket", {
+    const bucket = new Bucket(this, "ClientBucket", {
       publicReadAccess: true,
       websiteIndexDocument: "index.html",
+    });
+
+    const clientDistribution = new CloudFrontWebDistribution(
+      this,
+      "ClientDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+        defaultRootObject: "index.html",
+      }
+    );
+
+    new BucketDeployment(this, "ClientBucketDeployment", {
+      destinationBucket: bucket,
+      sources: [Source.asset(join(__dirname, "../client/dist"))],
+      distribution: clientDistribution,
+      distributionPaths: ["/index.html"],
     });
   }
 }
