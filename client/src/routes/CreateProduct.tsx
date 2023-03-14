8ed18a24c72fd6f81a4ae55d7486241076e8f14d
abhishandy <abhishek.sandy@gmail.com>
2023-03-13 23:42:23 -0400

    feat: use mutation

--- client/src/routes/CreateProduct.tsx
+++ client/src/routes/CreateProduct.tsx
@@ -2,27 +2,40 @@ import { SubmitHandler } from "react-hook-form";
 import AddProductForm, { AddProduct } from "../lib/AddProductForm";
 import axios from "axios";
 import { Navbar } from "../lib/Navbar";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 export const CreateProduct = () => {
+  const navigate = useNavigate();
+  const mutation = useMutation(
+    ({ data, base64str }: { data: AddProduct; base64str: string }) => {
+      const PRODUCTS_URL = import.meta.env.VITE_BASE_URL + "/products";
+      return axios.post(PRODUCTS_URL, {
+        name: data.name,
+        description: data.description,
+        images: [
+          {
+            type: data.image[0].type,
+            data: base64str,
+          },
+        ],
+        price: Number(data.price),
+      });
+    },
+    {
+      onSuccess: () => {
+        navigate("/");
+      },
+    }
+  );
+
   const onSubmit: SubmitHandler<AddProduct> = async (event) => {
     try {
-      const PRODUCTS_URL = import.meta.env.VITE_BASE_URL + "/products";
-
       const reader = new FileReader();
       reader.onload = async function (fileReaderEvent) {
         const result = fileReaderEvent.target?.result as string;
         const base64str = result.replace(/^data:image\/(png|jpeg);base64,/, "");
-        await axios.post(PRODUCTS_URL, {
-          name: event.name,
-          description: event.description,
-          images: [
-            {
-              type: event.image[0].type,
-              data: base64str,
-            },
-          ],
-          price: Number(event.price),
-        });
+        mutation.mutate({ data: event, base64str });
       };
       reader.readAsDataURL(event.image[0]);
     } catch (e) {
@@ -36,6 +49,9 @@ export const CreateProduct = () => {
       <div className="max-w-lg mx-auto mt-8 px-4">
         <AddProductForm onSubmit={onSubmit} />
       </div>
+      {mutation.isError && (
+        <div className="text-red-500 text-center">error creating product</div>
+      )}
     </>
   );
 };
