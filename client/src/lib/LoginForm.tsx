f7d371dc81c079ba7154e4c3e93356d35094d499
abhishandy <abhishek.sandy@gmail.com>
2023-03-14 00:01:51 -0400

    feat: nostr theme login form

--- client/src/lib/LoginForm.tsx
+++ client/src/lib/LoginForm.tsx
@@ -10,55 +10,39 @@ export default function Example() {
           ```
         */}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          {/* <img
+        {/* <div className="sm:mx-auto sm:w-full sm:max-w-md"> */}
+        {/* <img
             className="mx-auto h-12 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=gray&shade=600"
             alt="Your Company"
           /> */}
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+        {/* <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in
+          </h2> */}
+        {/* </div> */}
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label
-                  htmlFor="email"
+                  htmlFor="nostrkey"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  pubkey
+                  Nostr key
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="nostrkey"
+                    name="nostrkey"
+                    type="nostrkey"
+                    autoComplete="nostrkey"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                   />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  privkey
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-                  />
+                  <p className="text-xs text-gray-500 mt-1">
+                    priv key for full access, pub key for read-only access
+                  </p>
                 </div>
               </div>
 
@@ -71,6 +55,12 @@ export default function Example() {
                 </button>
               </div>
             </form>
+            <p className="text-center mt-4 text-sm">OR</p>
+            <div className="mt-4">
+              <button className="flex w-full justify-center rounded-md bg-gray-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+                Generate
+              </button>
+            </div>
           </div>
         </div>
       </div>
