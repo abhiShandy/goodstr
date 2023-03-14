20b0bace2ebfbf7d4463599dd9766d638cfc0e42
abhishandy <abhishek.sandy@gmail.com>
2023-03-13 23:42:37 -0400

    chore: make product fields required

--- client/src/lib/AddProductForm.tsx
+++ client/src/lib/AddProductForm.tsx
@@ -37,7 +37,7 @@ export default function AddProductForm({
               </label>
               <div className="mt-2 flex rounded-md shadow-sm">
                 <input
-                  {...register("name")}
+                  {...register("name", { required: true })}
                   type="text"
                   id="name"
                   autoComplete="name"
@@ -55,7 +55,7 @@ export default function AddProductForm({
               </label>
               <div className="mt-2">
                 <textarea
-                  {...register("description")}
+                  {...register("description", { required: true })}
                   id="description"
                   rows={3}
                   className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:py-1.5 sm:text-sm sm:leading-6"
@@ -66,32 +66,6 @@ export default function AddProductForm({
                 Write a few sentences about the product.
               </p>
             </div>
-
-            {/* <div className="sm:col-span-6">
-              <label
-                htmlFor="photo"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Photo
-              </label>
-              <div className="mt-2 flex items-center">
-                <span className="h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                  <svg
-                    className="h-full w-full text-gray-300"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <button
-                  type="button"
-                  className="ml-5 rounded-md bg-white py-1.5 px-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Change
-                </button>
-              </div>
-            </div> */}
 
             <div className="sm:col-span-6">
               <label
@@ -124,7 +98,7 @@ export default function AddProductForm({
                       <span>Upload an image</span>
                       <input
                         id="image"
-                        {...register("image")}
+                        {...register("image", { required: true })}
                         type="file"
                         accept="image/*"
                         className="sr-only"
@@ -148,7 +122,7 @@ export default function AddProductForm({
               <div className="mt-2 flex rounded-md shadow-sm">
                 <input
                   type="number"
-                  {...register("price")}
+                  {...register("price", { required: true })}
                   id="price"
                   autoComplete="price"
                   className="block w-full min-w-0 flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
@@ -165,18 +139,11 @@ export default function AddProductForm({
 
       <div className="pt-5">
         <div className="flex justify-end">
-          <button
-            type="button"
-            className="rounded-md bg-white py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            Cancel
-          </button>
-          <button
+          <input
+            value={"Add"}
             type="submit"
             className="ml-3 inline-flex justify-center rounded-md bg-gray-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
-          >
-            Save
-          </button>
+          />
         </div>
       </div>
     </form>
