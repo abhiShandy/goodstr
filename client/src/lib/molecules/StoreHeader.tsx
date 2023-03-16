66b068e84e7d8ed34275bf901d8907986982ba66
abhishandy <abhishek.sandy@gmail.com>
2023-03-16 19:46:24 -0400

    feat: bg on store header

--- client/src/lib/molecules/StoreHeader.tsx
+++ client/src/lib/molecules/StoreHeader.tsx
@@ -9,7 +9,7 @@ export type Props = {
 
 export default function StoreHeader({ store }: Props) {
   return (
-    <div className="bg-white pt-12 sm:pt-12">
+    <div className="bg-gray-200 py-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
