3ad27f7befffc6395ccaf190307a4bec5ca14cb2
abhishandy <abhishek.sandy@gmail.com>
2023-03-20 09:13:21 -0400

    fix: navbar links

--- client/src/lib/molecules/Navbar.tsx
+++ client/src/lib/molecules/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = ({ currentPage }: NavbarProps) => {
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center font-bold text-lg font-mono">
-                  <a href="/">TheGoodStr</a>
+                  <a href="/">GoodStr</a>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   {/* Current: "border-gray-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
@@ -170,7 +170,7 @@ export const Navbar = ({ currentPage }: NavbarProps) => {
               {/* Current: "bg-gray-50 border-gray-500 text-gray-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
-                href="/"
+                href="/discover"
                 className={
                   "block border-l-4 py-2 pl-3 pr-4 text-base font-medium" +
                   (currentPage === "discover"
