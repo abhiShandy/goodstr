9d385c50e85f9285e0e899f4a7e3e42d30dfb7d8
abhishandy <abhishek.sandy@gmail.com>
2023-03-28 03:58:30 -0400

    chore: prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-  }
-}
+    quotes: ["error", "double"],
+    "comma-dangle": "off",
+    semi: "off",
+  },
+};
